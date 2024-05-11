70c55e65fc34e66fcb8ce5a43c270d7e99bf3e58
KentaroKajiyama <kemushi555@icloud.com>
2024-05-12 00:13:37 +0900

    feature : assignment1-4,5  implemented optimal-knapsack algorithm.

--- assignment1/greedy-knapsack.cpp
+++ assignment1/greedy-knapsack.cpp
@@ -5,57 +5,62 @@
 #include <algorithm>
 #include <filesystem>
 
+#define NUM_SAMPLES 10000
+
 using namespace std;
 //----------------------------------------------------------------
 // Entry point
 //----------------------------------------------------------------
 int main(void) {
-  // Input
   ifstream file("input.txt");
   if (!file.is_open()) {
     cerr << "Error opening file" << endl;
     return 1;
   }
-  unordered_set<unsigned int> berry_indices;
-  unsigned int n;
-  unsigned long W;
-  if (!(file >> W >> n)) {
-    cerr << "Error reading w or n" << endl;
+  ofstream outFile("SOL.txt", ios::app);
+  if (!outFile){
+    cerr << "Error opening file" << endl;
     return 1;
   }
-  unsigned long happiness[n];
-  unsigned long weight[n];
-  for (unsigned int i = 0; i < n; i++) {
-    if (!(file >> happiness[i] >> weight[i])) {
-      cerr << "Error reading happiness or weight arrays" << endl;
+  for (int i = 0; i < NUM_SAMPLES; i++) {
+    // Input
+    unordered_set<unsigned int> berry_indices;
+    float W;
+    unsigned int n;
+    if (!(file >> W >> n)) {
+      cerr << "Error reading w or n" << endl;
       return 1;
     }
+    float happiness[n];
+    float weight[n];
+    for (unsigned int i = 0; i < n; i++) {
+      if (!(file >> happiness[i] >> weight[i])) {
+        cerr << "Error reading happiness or weight arrays" << endl;
+        return 1;
+      }
+    }
+    // Greedy algorithm
+    float sum_s_weight = 0;
+    float sum_s_happiness = 0;
+    for (int j = 0; j < n; j++) {
+      if ( sum_s_weight + weight[j] <= W ) {
+        berry_indices.insert(j);
+      } else {
+        if ( happiness[j] >= sum_s_happiness && weight[j] <= W ) {
+          berry_indices.clear();
+          berry_indices.insert(j);
+          sum_s_happiness = happiness[j];
+        }
+        break;
+      }
+      sum_s_weight += weight[j];
+      sum_s_happiness += happiness[j];
+    }
+    // Output
+    float SOL = sum_s_happiness;
+    outFile << SOL << endl;
   }
   file.close();
-  // Greedy algorithm
-  unsigned long sum_s_weight = 0;
-  unsigned long sum_s_happiness = 0;
-  for (int j = 0; j < n; j++) {
-    if ( sum_s_weight + weight[j] <= W ) {
-      berry_indices.insert(j);
-    } else {
-      if ( happiness[j] >= sum_s_happiness && weight[j] <= W ) {
-        berry_indices.clear();
-        berry_indices.insert(j);
-      }
-      break;
-    }
-    sum_s_weight += weight[j];
-    sum_s_happiness += happiness[j];
-  }
-  // Output
-  vector<unsigned int> outputs(berry_indices.begin(), berry_indices.end());
-  sort(outputs.begin(), outputs.end());
-  cout << "greedy solutions:";
-  for (int num: outputs) {
-    cout << num << " ";
-  }
-  cout << endl;
-
+  outFile.close();
   return 0;
 }
